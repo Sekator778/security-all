57aade34241434f22b2907a340d82d7479e0facf
Sekator778@ <sekator778@gmail.com>
2022-09-10 20:36:46 +0200

    change brunch

--- src/main/java/com/example/secur/controller/PageController.java
+++ src/main/java/com/example/secur/controller/PageController.java
@@ -1,2 +1,17 @@
-package com.example.secur.controller;public class PageController {
+package com.example.secur.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @Operation(summary = "home page")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "All good done")})
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
--- src/main/java/com/example/secur/security/SecurityConfig.java
+++ src/main/java/com/example/secur/security/SecurityConfig.java
@@ -1,2 +1,36 @@
-package com.example.secur.security;public class SecurityConfig {
+package com.example.secur.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> usersList = new ArrayList<>();
+        usersList.add(new User(
+
+                "admin", encoder.encode("admin"),
+
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+
+        usersList.add(new User(
+                "user", encoder.encode("user"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(usersList);
+    }
 }
